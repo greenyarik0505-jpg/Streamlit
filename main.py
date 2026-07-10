9573b30676985eda462a17f54e9498fcccfbcc60
greenyarik0505-jpg <greenyarik0505@gmail.com>
2026-07-10 17:30:13 +0300

    Add additional quiz questions and options

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import streamlit as st
 
 st.title("Проверка знаний на Дибил")
-
 fotball1 = st.radio("Оберіть Футболиста у кого больше всего голов:", ["Месси", "Роналду", "Неймар"])
-
 if fotball1 == "Роналду":
     st.success("Правильно")
 else:
     st.error("Неправильно")
-food = st.radio("Яка краща їжа:", ["Бургер", "Чипси", "макарони"])
 
+food = st.radio("Яка краща їжа:", ["Бургер", "Чипси", "макарони"])
 if food == "Чипси":
+    st.success("Правильно")
+else:
+    st.error("Неправильно")
+
+brainroti = st.radio("Який брейнрот акула?", ["Tralalelo Tralala", "Tung Tung Tung Sahur", "Br Br Patapim"])
+if brainroti == "Tralalelo Tralala":
+    st.success("Правильно")
+else:
+    st.error("Неправильно")
+
+strana = st.radio("Какая самая могущая страна била раньше:", ["СРСР", "США", "Немецкая Империя"])
+if strana == "СРСР":
     st.success("Правильно")
 else:
     st.error("Неправильно")
